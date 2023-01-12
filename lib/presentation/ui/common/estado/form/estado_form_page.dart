746ebcc8c66e2e35d9f94f6f238c4dff4bae8ca7
HandreiFontana <handreifontana@gmail.com>
2023-01-12 14:31:00 -0300

    feat (#466): adição da página de estado form

---
+++ lib/presentation/ui/common/estado/form/estado_form_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class EstadoFormPage extends StatefulWidget {
+  const EstadoFormPage({super.key});
+
+  @override
+  State<EstadoFormPage> createState() => _EstadoFormPageState();
+}
+
+class _EstadoFormPageState extends State<EstadoFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
